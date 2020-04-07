fdc97a99867190cc7a0271190c0c5b018181d92f
VlaTal1 <vladiktalah@gmail.com>
2020-04-07 18:16:04 +0300

    Custom.h Custom.cpp

---
+++ Lesson22/Custom.cpp
@@ -0,0 +1,45 @@
+#include "Custom.h"
+
+Custom::Custom()
+{
+	description = "none";
+}
+
+Custom::Custom(Date date, Time_ time, string description) : Event(date, time)
+{
+	this->description = description;
+}
+
+Custom::~Custom()
+{
+}
+
+void Custom::setDescription(string description)
+{
+	this->description = description;
+}
+
+string Custom::getDescription() const &
+{
+	return description;
+}
+
+void Custom::Show()
+{
+	cout << "Date: " << date << endl;
+	cout << "Time: " << time << endl;
+	cout << "Description: " << description << endl;
+	cout << "==============================================" << endl;
+}
+
+string Custom::toString()
+{
+	return "{Date: " + to_string(date.getDay()) + "-" + to_string(date.getMonth()) + "-" +
+		to_string(date.getYear()) + "; Time: " + to_string(time.getHour()) + ":" +
+		to_string(time.getMinutes()) + "; Description: " + description + ";}";
+}
+
+string Custom::Type()
+{
+	return "Custom";
+}
---
+++ Lesson22/Custom.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Event.h"
+class Custom : public Event
+{
+protected:
+	string description;
+
+public:
+	Custom();
+	Custom(Date date, Time_ time, string description);
+	~Custom();
+
+	void setDescription(string description);
+	string getDescription()const&;
+
+	virtual void Show();
+	virtual string toString();
+	virtual string Type();
+};
+
