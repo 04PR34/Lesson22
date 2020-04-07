db1f3338887f557abaee19881e4e3a0ad684152a
04PR34 <04pr34@gmail.com>
2020-04-07 17:09:10 +0300

    Event.cpp Event.h

--- Lesson22/Event.cpp
+++ Lesson22/Event.cpp
@@ -4,9 +4,37 @@
 
 Event::Event()
 {
+	date = Date();
+	time = Time_();
+}
+
+Event::Event(Date d, Time_ t)
+{
+	date = d;
+	time = t;
 }
 
 
 Event::~Event()
 {
+}
+
+void Event::setDate(Date d)
+{
+	date = d;
+}
+
+Date Event::getDate() const &
+{
+	return date;
+}
+
+void Event::setTime(Time_ t)
+{
+	time = t;
+}
+
+Time_ Event::getTime() const &
+{
+	return time;
 }
--- Lesson22/Event.h
+++ Lesson22/Event.h
@@ -9,9 +9,20 @@ class Event
 protected:
 	Date date;
 	Time_ time;
-
 public:
 	Event();
+	Event(Date d,Time_ t);
 	~Event();
+
+	void setDate(Date d);
+	Date getDate()const&;
+
+	void setTime(Time_ t);
+	Time_ getTime()const&;
+
+	virtual void Show()=0;
+	virtual void toString() = 0;
+	virtual string Type() = 0;
+
 };
 
